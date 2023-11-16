b30509401c71cf076994d4c1c84cf8b2a15cc1dd
Vlad Larin <lordgadyka71@gmail.com>
2023-11-16 19:19:02 +0200

    Implement User views & JWT authentication

--- airport_api/urls.py
+++ airport_api/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # path("api/cinema/", include("cinema.urls", namespace="cinema")),
+    path("api/user/", include("user.urls", namespace="user")),
 ]
