fadd8e0aa66ab238baab0595af988f2be4357e45
Vlad Larin <lordgadyka71@gmail.com>
2023-11-21 22:30:48 +0200

    Update settings.py to take values from .env

--- airport_api/settings.py
+++ airport_api/settings.py
@@ -21,14 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-hc*iyul^an_=rno&@nfd72w_zbbh4%46@tio6qa)@i&k50rnyx"
-)
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 INTERNAL_IPS = [
     "127.0.0.1",
