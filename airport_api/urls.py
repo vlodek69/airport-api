5a01cd33604246839e1a796222eaac0d690a0a49
Vlad Larin <lordgadyka71@gmail.com>
2023-11-22 18:14:17 +0200

    Reformat code style

--- airport_api/urls.py
+++ airport_api/urls.py
@@ -24,7 +24,11 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/airport/", include("airport.urls", namespace="airport")),
     path("api/user/", include("user.urls", namespace="user")),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/doc/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
     path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
