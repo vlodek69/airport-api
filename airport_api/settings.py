ee0ffa5a903cfd0029daa7022e5f372a5f228ebb
Vlad Larin <lordgadyka71@gmail.com>
2023-11-24 15:49:02 +0200

    Set ACCESS_TOKEN_LIFETIME to 30 minutes

--- airport_api/settings.py
+++ airport_api/settings.py
@@ -173,7 +173,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=500),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
 }
